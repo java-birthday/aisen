38ca107c93fb61fc2531729f64f2cedb66618cf6
heyanlong <yanlong.hee@gmail.com>
2019-05-23 17:15:30 +0800

    first commit

---
+++ src/main/java/com/birthday/aisen/AisenApplication.java
@@ -0,0 +1,13 @@
+package com.birthday.aisen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AisenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AisenApplication.class, args);
+	}
+
+}
