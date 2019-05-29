207da295596de79d4d80a4f78d36f39fcfb3c2ed
wycdahaoren <wycdahaoren@163.com>
2019-05-29 11:13:25 +0800

    增加swagger 插件，生成在线接口文档

--- src/main/java/com/birthday/aisen/controllers/UserController.java
+++ src/main/java/com/birthday/aisen/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.birthday.aisen.controllers;
 
 import com.birthday.aisen.service.IUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +13,13 @@ import com.birthday.aisen.dto.UserDTO;
 @Controller
 @RequestMapping("/api/user")
 @CrossOrigin()
+@Api(tags= "用户管理->获取用户信息")
 public class UserController {
 
     @Autowired
     private IUserService userService;
 
+    @ApiOperation(value = "获取用户信息", notes = "用户管理", httpMethod = "GET")
     @RequestMapping(value = "{uuid}", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity<UserDTO> info(@PathVariable String uuid) {
         UserDTO info = userService.getUserById(uuid);
